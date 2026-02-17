6919a762f2d4f17662e2b2f3903161c99c221964
haelamri <hadia.el-amri@capgemini.com>
2026-02-17 15:45:42 +0000

    backend - create main classe

---
+++ src/main/java/com/cabinetmedical/CabinetMedicalApplication.java
@@ -0,0 +1,16 @@
+package com.cabinetmedical;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Classe principale de l'application Cabinet Médical
+ */
+@SpringBootApplication
+public class CabinetMedicalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CabinetMedicalApplication.class, args);
+    }
+}
+
